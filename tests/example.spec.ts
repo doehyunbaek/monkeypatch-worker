d8a275179f4d0ea317cbadb9d9ea7a123bcb6bdc
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-09 09:41:50 +0900

    use api patch to monkey patch worker
    As suggested by https://github.com/microsoft/playwright/issues/28029

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,23 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-const monkeyPatch = () => {
+
+
+
+const monkeyPatch = `
   Math.random = () => 42;
-};
+`
 
 test('Page.workers @smoke', async function({ page}) {
-  page.on('console', msg => console.log(msg));
   await page.addInitScript(monkeyPatch);
-  // such api does not exist
-  // await worker.addInitScript(monkeyPatch);
-  
-  // while initializing the worker, the monkey patch is not applied.
+  await page.route('**/*.js', async route => {
+    const response = await route.fetch();
+    const script = await response.text();
+    await route.fulfill({ response, body: monkeyPatch + script });
+  });
   await Promise.all([
     page.waitForEvent('worker'),
     page.goto('http://localhost:5173/')]);
   const worker = page.workers()[0];
   
-  // this works but comes too late.
-  await worker.evaluate(monkeyPatch);
   expect(await page.evaluate(() => self['workerFunction']())).toBe('42');
   expect(await worker.evaluate(() => self['workerFunction']())).toBe('42');
 });
