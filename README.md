2777b5e1b558077732a9ba1632e943ffe14f3f1b
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-12 21:28:10 +0900

    make minimal showcase test

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # monkeypatching Web Worker with playwright
 
 This is a minimal repo that contains how to monkey patch workers with playwright.
-
-TODO: make test case work
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,17 +1,30 @@
 import { test, expect } from '@playwright/test';
 
+// Script that monkeypatches WebAssembly.instantiate to collect the buffer lengths
+const monkeyPatch = `
+let bufferLengths = []
+let originalInstantiate = WebAssembly.instantiate;
+WebAssembly.instantiate = function (buffer, importObject) {
+  bufferLengths.push(buffer.byteLength)
+  importObjects.push(JSON.stringify(importObject))
+  return originalInstantiate(buffer, importObject)
+};
+`
+
 test('Page.workers @smoke', async function({ page}) {
-  // await page.addInitScript(monkeyPatch);
-  // await page.route('**/*.js', async route => {
-  //   const response = await route.fetch();
-  //   const script = await response.text();
-  //   await route.fulfill({ response, body: monkeyPatch + script });
-  // });
-  // await Promise.all([
-  //   page.waitForEvent('worker'),
-  //   page.goto('http://localhost:5173/')]);
-  // const worker = page.workers()[0];
-  
-  // expect(await page.evaluate(() => self['workerFunction']())).toBe('42');
-  // expect(await worker.evaluate(() => self['workerFunction']())).toBe('42');
+  // Where actual script injection happens
+  await page.route('**/worker.js', async route => {
+    const response = await route.fetch();
+    const script = await response.text();
+    await route.fulfill({ response, body: monkeyPatch + script });
+  });
+
+  // Wait for the worker to be created
+  await Promise.all([
+    page.waitForEvent('worker'),
+    page.goto('http://localhost:5173/')]);
+  const worker = page.workers()[0];
+
+  // Now we get the collected buffer length
+  let bufferLengths = await expect.poll(async () => await worker.evaluate(() => bufferLengths)).toStrictEqual([41]);
 });
